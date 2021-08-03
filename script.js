252bcde4e72eebc29ff8863bec2e88c4c644fef3
josanjohnata <josanjohnata@gmail.com>
2021-08-03 11:34:25 -0300

    Add function check product info

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 // Essa função é para validar o Login
 
-const credentials = {
-  user: 'teste@teste.com',
-  password: '123456',
-};
+// const credentials = {
+//   user: 'teste@teste.com',
+//   password: '123456',
+// };
+// function login() {
+//   onclick="window.open('index.html', '_self')";
+// }
+// function checkLogin() {
+//   const inputName = document.getElementById('inputLogin').value;
+//   const inputPassword = document.getElementById('inputPassword').value;
+//   if (inputName === credentials.user && inputPassword === credentials.password) {
+//     alert('Olá! Esse é nosso sistema de estoque');
+//   } else {
+//     alert('Login ou senha inválidos.');
+//   }
+// }
 
-function checkLogin() {
-  const inputName = document.getElementById('inputLogin').value;
-  const inputPassword = document.getElementById('inputPassword').value;
-  if (inputName === credentials.user && inputPassword === credentials.password) {
-    alert('Olá! Esse é nosso sistema de estoque');
-  } else {
-    alert('Login ou senha inválidos.');
-  }
+// const buttonLogin = document.getElementById('buttonLogin');
+// buttonLogin.addEventListener('click', checkLogin);
+
+// Essa função valida as informações dos produtos.
+
+function checkProductInfo(txtNomeProduto, txtQuantidadeProduto, txtPrecoProduto, txtCodigoProduto) {
+  let productName = document.getElementById(txtNomeProduto).value;
+  let productQuantity = document.getElementById(txtQuantidadeProduto).value;
+  let productPrice = document.getElementById(txtPrecoProduto).value;
+  let productCode = document.getElementById(txtCodigoProduto).value;
+
+  if (productName == '') {
+    alert('Nome do produto está em branco. Por favor preenchê-lo!');
+  } else if (productQuantity == '') {
+    alert('A quantidade do produto está em branco. Por favor preenchê-lo!');
+  } else if (productPrice == '') {
+    alert('O preço do produto está em branco. Por favor preenchê-lo!');
+  } else if (productCode == '') {
+    alert('Nome do produto está em branco. Por favor preenchê-lo!');
+  } else registerProduct(productName, productQuantity, productPrice, productCode);
 }
-
-const buttonLogin = document.getElementById('buttonLogin');
-buttonLogin.addEventListener('click', checkLogin);
-
-
 
 
 
