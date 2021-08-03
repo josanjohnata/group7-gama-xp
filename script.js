3fbf0f1e0a02fd4934cb4f0000eae34c8da15dee
josanjohnata <josanjohnata@gmail.com>
2021-08-03 09:56:18 -0300

    Add login page

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-const api = require("./api");
+// Essa função é para validar o Login
 
-// exercício 2
-let destaqueTotal = 0;
-let total = 0;
+const credentials = {
+  user: 'teste@teste.com',
+  password: '123456',
+};
 
-function emDestaque() {
-  for (let i = 0; i < api.length; i += 1) {
-    if (listaProdutos.emDestaque === "sim") {
-      destaqueTotal = listaProdutos.emDestaque * listaProdutos.qtdEstoque;
-    }
-    return total + (listaProdutos.preco * destaqueTotal);
+function checkLogin() {
+  const inputName = document.getElementById('inputLogin').value;
+  const inputPassword = document.getElementById('inputPassword').value;
+  if (inputName === credentials.user && inputPassword === credentials.password) {
+    alert('Olá! Esse é nosso sistema de estoque');
+  } else {
+    alert('Login ou senha inválidos.');
   }
 }
+
+const buttonLogin = document.getElementById('buttonLogin');
+buttonLogin.addEventListener('click', checkLogin);
+
+
+
+
+
+
+
+
+
+// const api = require("./api");
+
+// // exercício 2
+// let destaqueTotal = 0;
+// let total = 0;
+
+// function emDestaque() {
+//   for (let i = 0; i < api.length; i += 1) {
+//     if (listaProdutos.emDestaque === "sim") {
+//       destaqueTotal = listaProdutos.emDestaque * listaProdutos.qtdEstoque;
+//     }
+//     return total + (listaProdutos.preco * destaqueTotal);
+//   }
+// }
 
 
